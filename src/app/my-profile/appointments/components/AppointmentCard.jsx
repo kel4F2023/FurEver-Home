1dad89015025dc54a2bbcea62ef43141adf69110
Scott(Xiaoyang) Chen <xiaoyan5@andrew.cmu.edu>
2024-12-02 23:27:03 -0800

    Adjust font-size for address and pet name fields, and enhance in-person avatar display effect

--- src/app/my-profile/appointments/components/AppointmentCard.jsx
+++ src/app/my-profile/appointments/components/AppointmentCard.jsx
@@ -6,10 +6,11 @@ const resecheduleInpersonAppointmentPath = '/check-pets/video-session';
 
 export function InpersonAppointmentCard({ id, date, time, address, cancelBtnHandler }) {
     return (
-        <div className='w-full max-w-md h-[122px] bg-white shadow-md mb-6 py-1 rounded-[15px]'>
-            <div className='h-[99px] w-full flex justify-between items-center px-2'>
-                <div className='inperson-avatar'>
-                    <p className='paragraph'>In-person Visit</p>
+        <div className='w-full max-w-md bg-white shadow-lg mb-6 pt-3 rounded-[15px]'>
+            <div className='h-[99px] w-full flex justify-between items-center px-3'>
+                <div className='appointment-card-avatar flex flex-col justify-center items-center'>
+                    <h3 className='H3'>In-person</h3>
+                    <h3 className='H3'>Visit</h3>
                 </div>
                 <div>
                     <p className="paragraph text-center">{date}</p>
@@ -24,8 +25,8 @@ export function InpersonAppointmentCard({ id, date, time, address, cancelBtnHand
                     }}>Cancel</button>
                 </div>
             </div>
-            <div className='bg-[#97ecf6]/25 px-2'>
-                <p className="paragraph">{address}</p>
+            <div className='bg-[#97ecf6]/25 px-3 rounded-[15px]'>
+                <h3 className="H3">Address: {address}</h3>
             </div>
         </div>
     )
@@ -34,14 +35,14 @@ export function InpersonAppointmentCard({ id, date, time, address, cancelBtnHand
 
 export function VideoAppointmentCard({ id, date, time, petName, avatarPath, cancelBtnHandler }) {
     return (
-        <div className='w-full max-w-md bg-white shadow-md mb-6 p-2 rounded-[15px]'>
+        <div className='w-full max-w-md bg-white shadow-lg mb-6 pt-3 px-3 rounded-[15px]'>
             <div className='w-full flex justify-between items-center'>
                 <div>
-                    <img className='w-[99px] h-[99px] rounded-[15px]' src={avatarPath} alt="pet's avatar" />
+                    <img className='appointment-card-avatar' src={avatarPath} alt="pet's avatar" />
                 </div>
                 <div>
-                    <p className="text-black text-sm font-medium font-['Inter'] text-center">{date}</p>
-                    <p className="text-black text-sm font-normal font-['Inter'] text-center">{time}</p>
+                    <p className="paragraph text-center">{date}</p>
+                    <p className="paragraph text-center">{time}</p>
                 </div>
                 <div className='h-[99px] flex flex-col justify-between'>
                     <Link className='block' href="">
@@ -56,7 +57,7 @@ export function VideoAppointmentCard({ id, date, time, petName, avatarPath, canc
                 </div>
             </div>
             <div>
-                <p className="paragraph relative left-7">{petName}</p>
+                <h3 className="H3 relative left-7">Pet Name: {petName}</h3>
             </div>
         </div>
     )
