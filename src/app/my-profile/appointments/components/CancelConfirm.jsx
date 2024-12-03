6b11ea5c3604064ac50158c1a74b17eb689bb759
Scott(Xiaoyang) Chen <xiaoyan5@andrew.cmu.edu>
2024-12-02 22:35:20 -0800

    Enhance popup UI on All Appointments page for better user experience

--- src/app/my-profile/appointments/components/CancelConfirm.jsx
+++ src/app/my-profile/appointments/components/CancelConfirm.jsx
@@ -2,19 +2,22 @@
 
 export default function CancelConfirm({ cancelCancelHandler, confimCancelHandler }) {
     return (
-        <div className="w-[276px] h-[130px] relative bg-white shadow">
-            <div className="w-[209px] h-[42px] left-[33px] top-[16px] absolute text-center">
-                <span className="paragraph">You confirmed</span>
-                <span className="paragraph"> canceling </span>
-                <span className="paragraph">your video session with</span>
-                <span className="paragraph"> Lucas on Nov 16, 2024, at 9:40 AM.</span>
+        <div className="w-[276px] bg-white shadow rounded-[15px] p-4 relative flex flex-col items-center justify-center">
+            <div className="w-full h-[42px] text-center">
+                <span className="font-sans font-normal">You confirmed</span>
+                <span className="font-sans font-semibold"> canceling </span>
+                <span className="font-sans font-normal">your video session with</span>
+                <span className="font-sans font-semibold"> Lucas on Nov 16, 2024, at 9:40 AM.</span>
             </div>
-            <div className="w-11 h-[21px] p-3 left-[67px] top-[92px] absolute bg-[#b1b1b1] rounded-lg border border-[#2c2c2c] justify-center items-center gap-2 inline-flex">
-                <button onClick={cancelCancelHandler} className="paragraph text-[#2c2c2c] leading-none tracking-wide">Back</button>
-            </div>
-            <div className="w-11 h-[21px] p-3 left-[152px] top-[92px] absolute bg-[#2c2c2c] rounded-lg border border-[#2c2c2c] justify-center items-center gap-2 inline-flex">
-                <button onClick={confimCancelHandler} className="paragraph text-neutral-100 leading-none tracking-wide">Yes</button>
+            <div className="flex justify-evenly w-full mt-10">
+                <div className="pl-4 pr-5 py-1 justify-center items-center gap-3 inline-flex bg-white border border-primaryBlue rounded-lg">
+                    <button onClick={cancelCancelHandler} className="text-center text-[#00a9ff] text-normal font-semibold font-['Open Sans']">Back</button>
+                </div>
+                <div className="pl-4 pr-5 py-1 justify-center items-center gap-3 inline-flex bg-[#00A9FF] rounded-lg">
+                    <button onClick={confimCancelHandler} className="text-center text-white text-normal font-semibold font-['Open Sans']">Yes</button>
+                </div>
             </div>
         </div>
     );
 }
+
